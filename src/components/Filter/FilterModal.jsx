f40844f8278cf0dad9b429326cd611374a6aa7ee
Bazc83 <bazc916@gmail.com>
2022-11-28 16:43:42 +0000

    Working on FilterModal

--- src/components/Filter/FilterModal.jsx
+++ src/components/Filter/FilterModal.jsx
@@ -1,9 +1,26 @@
 import styles from '@styles/Filter.module.css';
 export const FilterModal = ({ light }) => {
-  console.log(light);
+  const handleModalClick = (e) => {
+    e.stopPropagation();
+  };
   return (
-    <div className={`${light ? styles.filterModalLight : styles.filterModal}`}>
-      FilterModal
+    <div
+      className={`${light ? styles.filterModalLight : styles.filterModal}`}
+      onClick={handleModalClick}>
+      <form className={styles.filterModalForm}>
+        <div className={styles.filterModalFormItem}>
+          <input type='checkbox' name='draft' />
+          <label htmlFor='draft'>Draft</label>
+        </div>
+        <div className={styles.filterModalFormItem}>
+          <input type='checkbox' name='pending' />
+          <label htmlFor='pending'>Pending</label>
+        </div>
+        <div className={styles.filterModalFormItem}>
+          <input type='checkbox' name='paid' />
+          <label htmlFor='paid'>Paid</label>
+        </div>
+      </form>
     </div>
   );
 };
