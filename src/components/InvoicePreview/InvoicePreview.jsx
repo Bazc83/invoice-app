da7ee80c9aecd42df7b2f9cc28756b7f9f1e91ce
Bazc83 <bazc916@gmail.com>
2022-11-29 20:47:22 +0000

    Working on InvoicePreview CSS

--- src/components/InvoicePreview/InvoicePreview.jsx
+++ src/components/InvoicePreview/InvoicePreview.jsx
@@ -3,16 +3,17 @@ export const InvoicePreview = (props) => {
   const { invoiceRef, name, dueDate, amount, paymentStatus } = props;
   return (
     <div className={styles.invoicePreview}>
+
       <h4 className={styles.invoiceRef}>
         <span className={styles.invoiceRefHash}>#</span>
         {invoiceRef}
       </h4>
 
-      <p className={` ${styles.dueDate}`}>{dueDate}</p>
+      <p className={`text ${styles.dueDate}`}>{dueDate}</p>
 
       <p className={`text ${styles.name}`}>{name}</p>
 
-      <h3 className={`${styles.amount}`}>£{amount}</h3>
+      <h3 className={`${styles.amount}`}>£ {amount}</h3>
 
       <div className={styles.paymentStatusPaid}>
         <div className={styles.customBulletPaid}></div>
@@ -22,8 +23,4 @@ export const InvoicePreview = (props) => {
   );
 };
 
-// invoiceRef={'RT3080'}
-// name={'Jensen Huang'}
-// dueDate={'Due  20 Sep 2021'}
-// amount={1800.9}
-// paymentStatus={"paid"}
+
