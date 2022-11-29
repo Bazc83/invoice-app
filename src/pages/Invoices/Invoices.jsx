24546d4220a62a4ed042e83fcedc9f89d6ddea89
Bazc83 <bazc916@gmail.com>
2022-11-29 21:13:39 +0000

    Updating CSS Invoices page

--- src/pages/Invoices/Invoices.jsx
+++ src/pages/Invoices/Invoices.jsx
@@ -2,6 +2,8 @@ import { DarkModeContext } from '@/App';
 import { InvoicePreview } from '@/components/InvoicePreview';
 import { useContext } from 'react';
 import { InvoicesPageControls } from './InvoicesPageControls';
+
+import styles from './styles/Invoices.module.css';
 export const Invoices = () => {
   const { light } = useContext(DarkModeContext);
 
@@ -9,13 +11,43 @@ export const Invoices = () => {
     <div className='container'>
       <InvoicesPageControls light={light} />
 
-      <InvoicePreview
-        invoiceRef={'RT3080'}
-        name={'Jensen Huang'}
-        dueDate={'Due  20 Sep 2021'}
-        amount={'1,800.90'}
-        paymentStatus={'paid'}
-      />
+      <div className={styles.invoicesWrapper}>
+        <InvoicePreview
+          invoiceRef={'RT3080'}
+          name={'Jensen Huang'}
+          dueDate={'Due  20 Sep 2021'}
+          amount={'1,800.90'}
+          paymentStatus={'paid'}
+        />
+        <InvoicePreview
+          invoiceRef={'XM9141'}
+          name={'Alex Grimm'}
+          dueDate={'Due 20 Sep 2021'}
+          amount={'556.00'}
+          paymentStatus={'pending'}
+        />
+        <InvoicePreview
+          invoiceRef={'RG0314'}
+          name={'John Morrison'}
+          dueDate={'Due 01 Oct 2021'}
+          amount={'14,002.33'}
+          paymentStatus={'paid'}
+        />
+        <InvoicePreview
+          invoiceRef={'RT2080'}
+          name={'Alysa Werner'}
+          dueDate={'Due 12 Oct 2021'}
+          amount={'102.04'}
+          paymentStatus={'pending'}
+        />
+        <InvoicePreview
+          invoiceRef={'FV2353'}
+          name={'Anita Wainwright'}
+          dueDate={'Due 12 Nov 2021'}
+          amount={'3,102.04'}
+          paymentStatus={'draft'}
+        />
+      </div>
     </div>
   );
 };
