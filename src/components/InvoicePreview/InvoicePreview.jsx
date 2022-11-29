6261202e28bdad374ef689b4639d37331f147a9e
Bazc83 <bazc916@gmail.com>
2022-11-29 21:33:26 +0000

    Updating CSS for InvoicePreview

--- src/components/InvoicePreview/InvoicePreview.jsx
+++ src/components/InvoicePreview/InvoicePreview.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import styles from './styles/InvoicePreview.module.css';
 export const InvoicePreview = (props) => {
   const { invoiceRef, name, dueDate, amount, paymentStatus } = props;
+
+  let paymentStatusStyling;
+  if (paymentStatus === 'paid') {
+    paymentStatusStyling = styles.paymentStatusPaid;
+  } else if (paymentStatus === 'pending') {
+    paymentStatusStyling = styles.paymentStatusPending;
+  } else if (paymentStatus === 'draft') {
+    paymentStatusStyling = styles.paymentStatusDraft;
+  }
+
   return (
     <div className={styles.invoicePreview}>
-
       <h4 className={styles.invoiceRef}>
         <span className={styles.invoiceRefHash}>#</span>
         {invoiceRef}
@@ -15,12 +25,10 @@ export const InvoicePreview = (props) => {
 
       <h3 className={`${styles.amount}`}>£ {amount}</h3>
 
-      <div className={styles.paymentStatusPaid}>
-        <div className={styles.customBulletPaid}></div>
-        <p className={styles.paymentStatusTextPaid}>Paid</p>
+      <div className={paymentStatusStyling}>
+        <div className={styles.customBullet}></div>
+        <p>{paymentStatus}</p>
       </div>
     </div>
   );
 };
-
-
