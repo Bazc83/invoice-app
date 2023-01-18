a06fdac598e588db0fb5d1f3aff0386629f59a12
Bazc83 <bazc916@gmail.com>
2023-01-18 16:15:41 +0000

    Started adding authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,13 @@
+const User = require('../models/userModel');
+
+// Login user
+const loginUser = async (req, res) => {
+  res.json({ message: 'login user' });
+};
+
+// Signup user
+const signupUser = async (req, res) => {
+  res.json({ message: 'signup user' });
+};
+
+module.exports = { loginUser, signupUser };
--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,19 @@ connectDB();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors({origin: false}));
+app.use(cors({ origin: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
+// user route
+app.use('/api/user', require('./routes/user'));
+
+// Invoices
 app.use('/api/invoices', require('./routes/invoiceRoutes'));
+
+// Invoice id routes
 app.use('/api/invoiceId', require('./routes/invoiceIdRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
