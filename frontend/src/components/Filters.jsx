7da88ebb6bdf8d368f34ce28a29a2fd8f9da2608
Bazc83 <bazc916@gmail.com>
2023-02-18 13:49:53 +0000

    Added a useEffect to clear filter state

--- frontend/src/components/Filters.jsx
+++ frontend/src/components/Filters.jsx
@@ -1,17 +1,19 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { InvoicesContext } from '@/context/InvoicesContext';
 
 import FilterItemCheckbox from './FilterItemCheckbox';
 
 function Filters() {
-  const { state } = useContext(InvoicesContext);
+  const { state, dispatch } = useContext(InvoicesContext);
+
+  useEffect(() => dispatch({ type: 'clearFilters' }), [dispatch]);
 
   return (
-    <div className="flex  gap-3 px-2  md:gap-6 lg:flex-col text-sm">
+    <div className="flex  gap-3 px-2  text-sm md:gap-6 lg:flex-col">
       <h1>Filters:</h1>
 
-      <form className="flex gap-3 lg:flex-col flex-wrap">
+      <form className="flex flex-wrap gap-3 lg:flex-col">
         {/* Filter modal options */}
         {state.filters.map((filter) => (
           <FilterItemCheckbox filter={filter} key={filter.filterValue} />
