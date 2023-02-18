f3f55b9d236f0abeb856b2bf0303bee1e71b604d
Bazc83 <bazc916@gmail.com>
2023-02-18 13:20:50 +0000

    Updated

--- backend/server.js
+++ backend/server.js
@@ -17,8 +17,6 @@ app.use(cors({ origin: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
 // user route
 app.use('/api/user', require('./routes/userRoutes'));
 
@@ -28,4 +26,16 @@ app.use('/api/invoices', require('./routes/invoiceRoutes'));
 // Invoice id routes
 app.use('/api/invoiceId', require('./routes/invoiceIdRoutes'));
 
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 app.listen(port, () => console.log(`Server started on port ${port}`));
